21f9bd96a2de1f3b21e603fb61b82ab9c91d4acc
Thien My <myntuit@gmail.com>
2021-11-09 08:17:49 +0900

    Refactor31-next-permutation; Add array reverse util

--- src/leetcode/other/31-next-permutation.kt
+++ src/leetcode/other/31-next-permutation.kt
@@ -1,25 +1,18 @@
 package leetcode.other
 
+import util.reverse
 import util.swap
 
 class NextPermutation {
     private fun nextPermutation(nums: IntArray) {
-        var i = nums.size-1
-        while (i > 0) {
-            if (nums[i-1] < nums[i]) {
-                nums.sort(i)
-                for (j in i until nums.size) {
-                    if (nums[j] > nums[i-1]) {
-                        nums.swap(i-1, j)
-                        break;
-                    }
-                }
-                break
-            }
-            --i
+        var i = nums.size-2
+        while (i >= 0 && nums[i] >= nums[i+1]) --i
+        if (i >= 0) {
+            var j = nums.size-1
+            while (j > i && nums[j] <= nums[i]) --j
+            nums.swap(i, j)
         }
-
-        if (i == 0) nums.sort()
+        nums.reverse(i+1)
     }
 
     operator fun invoke(nums: IntArray) {
--- src/util/Ext.kt
+++ src/util/Ext.kt
@@ -6,3 +6,19 @@ package util
 fun IntArray.swap(i: Int, j: Int) {
     this[i] = this[j].also { this[j] = this[i] }
 }
+
+/**
+ * Reverse elements from [start] to [end]
+ */
+fun IntArray.reverse(start: Int, end: Int) {
+    var l = start
+    var r = end
+    while (l < r) swap(l++, r--)
+}
+
+/**
+ * Reverse elements from [start] to the end of array
+ */
+fun IntArray.reverse(start: Int) {
+    reverse(start, size-1)
+}
