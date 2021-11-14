4870efc738ce2faa80fb80c18d3efda8e17d95fa
My Nguyen <myntuit@gmail.com>
2021-11-15 08:39:56 +0900

    Update src/leetcode/graph/1971-find-if-path-exists-in-graph.kt

--- src/leetcode/graph/1971-find-if-path-exists-in-graph.kt
+++ src/leetcode/graph/1971-find-if-path-exists-in-graph.kt
@@ -1,27 +1,30 @@
 package leetcode.graph
 
-class FindIfPathExistsInGraph {
+class FindIfPathExistsInGraph() {
 
-    fun dfs(graph: Array<MutableList<Int>>, visited: BooleanArray, start: Int, end: Int): Boolean {
+    private lateinit var graph: Array<MutableList<Int>>
+    private lateinit var visited: BooleanArray
+
+    private fun dfs(start: Int, end: Int): Boolean {
         if (start == end) return true
         for (v in graph[start]) {
             if (!visited[v]) {
                 visited[v] = true
-                if (dfs(graph, visited, v, end)) return true
+                if (dfs(v, end)) return true
             }
         }
         return false
     }
 
     private fun validPath(n: Int, edges: Array<IntArray>, start: Int, end: Int): Boolean {
-        val graph: Array<MutableList<Int>> = Array(n) { mutableListOf<Int>() }
+        graph = Array(n) { mutableListOf<Int>() }
         for (e in edges) {
             graph[e[0]].add(e[1])
             graph[e[1]].add(e[0])
         }
-        val visited = BooleanArray(n)
+        visited = BooleanArray(n)
         visited[start] = true
-        return dfs(graph, visited, start, end)
+        return dfs(start, end)
     }
 
     operator fun invoke(n: Int, edges: Array<IntArray>, start: Int, end: Int): Boolean {
