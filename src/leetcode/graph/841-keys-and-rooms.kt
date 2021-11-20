59810355f7be72b82e68a732c5662af209a52ba6
Thien My <myntuit@gmail.com>
2021-11-20 16:41:53 +0900

    Add leetcode/graph/841-keys-and-rooms.kt - BFS

---
+++ src/leetcode/graph/841-keys-and-rooms.kt
@@ -0,0 +1,27 @@
+package leetcode.graph
+
+import java.util.*
+
+class KeysAndRooms {
+
+    private fun canVisitAllRooms(rooms: List<List<Int>>): Boolean {
+        val visited = BooleanArray(rooms.size)
+        val queue = LinkedList<Int>()
+        queue.add(0)
+        while (queue.isNotEmpty()) {
+            var n = queue.size
+            while (n-- > 0) {
+                val front = queue.remove()
+                visited[front] = true
+                for (v in rooms[front]) {
+                    if (!visited[v]) queue.add(v)
+                }
+            }
+        }
+        return visited.count { it } == rooms.size
+    }
+
+    operator fun invoke(rooms: List<List<Int>>): Boolean {
+        return canVisitAllRooms(rooms)
+    }
+}
