707252048dc4d3dee830f3d12d99362f2396a1e2
Thien My <myntuit@gmail.com>
2021-11-27 19:27:49 +0900

    Update the union-find implementation of 947-most-stones-removed

--- src/leetcode/union_find/947-most-stones-removed.kt
+++ src/leetcode/union_find/947-most-stones-removed.kt
@@ -2,61 +2,29 @@ package leetcode.union_find
 
 class MostStonesRemovedWithSameRowOrColumn {
 
-    private fun find(p: IntArray, v: Int): Int {
-        return if (p[v] == v) v else find(p, p[v])
+    val p = HashMap<Int, Int>()
+    var count = 0
+
+    private fun find(u: Int): Int {
+        if (!p.containsKey(u)) p[u] = u
+        if (u != p[u]) p[u] = find(p[u]!!)
+        return p[u]!!
     }
 
-    private fun unite(p: IntArray, u: Int, v: Int) {
-        p[find(p, u)] = find(p, v)
-    }
-
-    private fun dfs(graph: Array<MutableList<Int>>, p: IntArray, start: Int) {
-        graph[start].forEach {
-            if (find(p, it) != find(p, start)) {
-                unite(p, it, start)
-                dfs(graph, p, it)
-            }
+    private fun unite(u: Int, v: Int) {
+        val x = find(u)
+        val y = find(v)
+        if (x != y) {
+            p[x] = y
+            ++count
         }
     }
 
     private fun removeStones(stones: Array<IntArray>): Int {
-        val n = stones.size
-        val graph: Array<MutableList<Int>> = Array(n) { mutableListOf() }
-
-        val xMap = HashMap<Int, MutableList<Int>>()
-        val yMap = HashMap<Int, MutableList<Int>>()
-
-        stones.forEachIndexed { idx, stone ->
-            if (xMap[stone[0]] != null) xMap[stone[0]]!!.add(idx)
-            else xMap[stone[0]] = mutableListOf(idx)
-
-            if (yMap[stone[1]] != null) yMap[stone[1]]!!.add(idx)
-            else yMap[stone[1]] = mutableListOf(idx)
+        stones.forEach { stone ->
+            unite(stone[0], stone[1].inv())
         }
-
-        stones.forEachIndexed { idx, stone ->
-            xMap[stone[0]]?.let {
-                it.forEach { point ->
-                    if (point != idx) graph[idx].add(point)
-                }
-            }
-
-            yMap[stone[1]]?.let {
-                it.forEach { point ->
-                    if (point != idx) graph[idx].add(point)
-                }
-            }
-        }
-
-        val p = (0 until n).toList().toIntArray()
-        for (i in 0 until n) dfs(graph, p, i)
-
-        var res = n
-        p.forEachIndexed { idx, v ->
-            if (idx == v) --res
-        }
-
-        return res
+        return stones.size - (p.size - count)
     }
 
     operator fun invoke(stones: Array<IntArray>): Int = removeStones(stones)
