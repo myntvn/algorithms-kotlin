61f20e68437fd63802bb81ad9ec13f511ba6e232
My Nguyen <myntuit@gmail.com>
2023-03-18 12:21:19 +0900

    Add DP implementation for 935-knight-dialer

--- src/main/kotlin/leetcode/dp/935-knight-dialer.kt
+++ src/main/kotlin/leetcode/dp/935-knight-dialer.kt
@@ -2,41 +2,62 @@ package leetcode.dp
 
 class KnightDialer {
     private val mod = (1e9+7).toInt()
-    private lateinit var neighbors: HashMap<Int, IntArray>
-    private lateinit var dp: Array<IntArray>
 
-    private fun dfs(count: Int, start: Int): Int {
+    private val neighbors = hashMapOf(
+        0 to intArrayOf(4, 6),
+        1 to intArrayOf(6, 8),
+        2 to intArrayOf(7, 9),
+        3 to intArrayOf(4, 8),
+        4 to intArrayOf(9, 3, 0),
+        5 to intArrayOf(),
+        6 to intArrayOf(1, 7, 0),
+        7 to intArrayOf(2, 6),
+        8 to intArrayOf(1, 3),
+        9 to intArrayOf(4, 2),
+    )
+
+    private fun dfs(count: Int, start: Int, memo: Array<IntArray>): Int {
         if (count == 0) return 1;
-        if (dp[start][count] != -1) return dp[start][count]
+        if (memo[start][count] != -1) return memo[start][count]
 
         var res = 0
         for (i in neighbors[start]!!) {
-            res += dfs(count-1, i)
+            res += dfs(count-1, i, memo)
             res %= mod
         }
-        dp[start][count] = res
+        memo[start][count] = res
         return res
     }
 
-    fun knightDialer(n: Int): Int {
-        neighbors = hashMapOf(
-            1 to intArrayOf(6, 8),
-            2 to intArrayOf(7, 9),
-            3 to intArrayOf(4, 8),
-            4 to intArrayOf(9, 3, 0),
-            5 to intArrayOf(),
-            6 to intArrayOf(1, 7, 0),
-            7 to intArrayOf(2, 6),
-            8 to intArrayOf(1, 3),
-            9 to intArrayOf(4, 2),
-            0 to intArrayOf(4, 6)
-        )
+    fun knightDialerMemoization(n: Int): Int {
 
-        dp = Array(10) { IntArray(n) { -1 } }
+        val memo = Array(10) { IntArray(n) { -1 } }
 
         var res = 0
         for (i in 0 until 10) {
-            res += dfs(n-1, i)
+            res += dfs(n-1, i, memo)
+            res %= mod
+        }
+
+        return res
+    }
+
+    fun knightDialerDp(n: Int): Int {
+        var dp = IntArray(10) { 1 }
+        repeat(n-1) {
+            val tmp = IntArray(10)
+            for (i in 0 until 10) {
+                neighbors[i]?.forEach {
+                    tmp[i] += dp[it]
+                    tmp[i] %= mod
+                }
+            }
+            dp = tmp
+        }
+
+        var res = 0
+        dp.forEach {
+            res += it
             res %= mod
         }
 
@@ -44,7 +65,7 @@ class KnightDialer {
     }
 
     operator fun invoke(n: Int): Int {
-        return knightDialer(n)
+        return knightDialerDp(n)
     }
 }
 
